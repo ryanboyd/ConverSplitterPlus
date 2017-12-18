ca87ad78911fe1af5d2f7fa5dbb6038d86d2ee7b
Ryan L. Boyd <ryanb@ryanb.cc>
2017-12-18 09:26:07 -0600

    Initial Commit

---
+++ WindowsFormsApplication1/MainForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BgWorker = new System.ComponentModel.BackgroundWorker();
+            this.ScanSubfolderCheckbox = new System.Windows.Forms.CheckBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.FolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.FilenameLabel = new System.Windows.Forms.Label();
+            this.SpeakerListTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EncodingDropdown = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SpeakersMultipleLinesCheckbox = new System.Windows.Forms.CheckBox();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.DetectSpeakersButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BgWorker
+            // 
+            this.BgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BgWorker_DoWork);
+            this.BgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BgWorker_RunWorkerCompleted);
+            // 
+            // ScanSubfolderCheckbox
+            // 
+            this.ScanSubfolderCheckbox.AutoSize = true;
+            this.ScanSubfolderCheckbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScanSubfolderCheckbox.Location = new System.Drawing.Point(33, 426);
+            this.ScanSubfolderCheckbox.Name = "ScanSubfolderCheckbox";
+            this.ScanSubfolderCheckbox.Size = new System.Drawing.Size(124, 20);
+            this.ScanSubfolderCheckbox.TabIndex = 2;
+            this.ScanSubfolderCheckbox.Text = "Scan subfolders";
+            this.ScanSubfolderCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.Location = new System.Drawing.Point(33, 381);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(152, 34);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FolderBrowser
+            // 
+            this.FolderBrowser.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.FolderBrowser.ShowNewFolderButton = false;
+            // 
+            // FilenameLabel
+            // 
+            this.FilenameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FilenameLabel.Location = new System.Drawing.Point(33, 487);
+            this.FilenameLabel.Name = "FilenameLabel";
+            this.FilenameLabel.Size = new System.Drawing.Size(319, 15);
+            this.FilenameLabel.TabIndex = 6;
+            this.FilenameLabel.Text = "Waiting to split texts...";
+            // 
+            // SpeakerListTextBox
+            // 
+            this.SpeakerListTextBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SpeakerListTextBox.Location = new System.Drawing.Point(33, 37);
+            this.SpeakerListTextBox.MaxLength = 2147483647;
+            this.SpeakerListTextBox.Multiline = true;
+            this.SpeakerListTextBox.Name = "SpeakerListTextBox";
+            this.SpeakerListTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.SpeakerListTextBox.Size = new System.Drawing.Size(319, 265);
+            this.SpeakerListTextBox.TabIndex = 7;
+            this.SpeakerListTextBox.Text = "Participant 1:\r\nParticipant 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(270, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "List of Speaker Tags (Case Sensitive)";
+            // 
+            // EncodingDropdown
+            // 
+            this.EncodingDropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.EncodingDropdown.FormattingEnabled = true;
+            this.EncodingDropdown.Location = new System.Drawing.Point(33, 343);
+            this.EncodingDropdown.Name = "EncodingDropdown";
+            this.EncodingDropdown.Size = new System.Drawing.Size(180, 21);
+            this.EncodingDropdown.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 324);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Encoding of Text Files:";
+            // 
+            // SpeakersMultipleLinesCheckbox
+            // 
+            this.SpeakersMultipleLinesCheckbox.AutoSize = true;
+            this.SpeakersMultipleLinesCheckbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SpeakersMultipleLinesCheckbox.Location = new System.Drawing.Point(33, 449);
+            this.SpeakersMultipleLinesCheckbox.Name = "SpeakersMultipleLinesCheckbox";
+            this.SpeakersMultipleLinesCheckbox.Size = new System.Drawing.Size(271, 20);
+            this.SpeakersMultipleLinesCheckbox.TabIndex = 11;
+            this.SpeakersMultipleLinesCheckbox.Text = "Speakers can have Multiple Lines of Text";
+            this.SpeakersMultipleLinesCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.FileName = "Repeatalizer.csv";
+            this.saveFileDialog.Filter = "CSV Files|*.csv";
+            this.saveFileDialog.Title = "Please choose where to save your output";
+            // 
+            // DetectSpeakersButton
+            // 
+            this.DetectSpeakersButton.Enabled = false;
+            this.DetectSpeakersButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DetectSpeakersButton.Location = new System.Drawing.Point(193, 381);
+            this.DetectSpeakersButton.Name = "DetectSpeakersButton";
+            this.DetectSpeakersButton.Size = new System.Drawing.Size(152, 34);
+            this.DetectSpeakersButton.TabIndex = 12;
+            this.DetectSpeakersButton.Text = "Detect Speakers";
+            this.DetectSpeakersButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(384, 511);
+            this.Controls.Add(this.DetectSpeakersButton);
+            this.Controls.Add(this.SpeakersMultipleLinesCheckbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.EncodingDropdown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SpeakerListTextBox);
+            this.Controls.Add(this.FilenameLabel);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.ScanSubfolderCheckbox);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 550);
+            this.MinimumSize = new System.Drawing.Size(400, 550);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ConverSplitterPlus!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker BgWorker;
+        private System.Windows.Forms.CheckBox ScanSubfolderCheckbox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.FolderBrowserDialog FolderBrowser;
+        private System.Windows.Forms.Label FilenameLabel;
+        private System.Windows.Forms.TextBox SpeakerListTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox EncodingDropdown;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox SpeakersMultipleLinesCheckbox;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Button DetectSpeakersButton;
+    }
+}
+
---
+++ WindowsFormsApplication1/MainForm.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+
+
+        //this is what runs at initialization
+        public Form1()
+        {
+
+            InitializeComponent();
+            
+
+            foreach(var encoding in Encoding.GetEncodings())
+            {
+                EncodingDropdown.Items.Add(encoding.Name);
+            }
+            EncodingDropdown.SelectedIndex = EncodingDropdown.FindStringExact(Encoding.Default.BodyName);
+
+
+        }
+
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowser.Description = "Please choose the location of your INPUT .txt files that you want to split";
+            FolderBrowser.ShowDialog();
+            string TextFileFolder = FolderBrowser.SelectedPath.ToString();
+
+            if (TextFileFolder != "")
+            {
+
+                FolderBrowser.Description = "Please choose the OUTPUT location for your files";
+
+                FolderBrowser.ShowDialog();
+                string OutputFileLocation = FolderBrowser.SelectedPath.ToString();
+
+
+                if (OutputFileLocation != "") { 
+                    StartButton.Enabled = false;
+                    SpeakerListTextBox.Enabled = false;
+                    ScanSubfolderCheckbox.Enabled = false;
+                    EncodingDropdown.Enabled = false;
+                    SpeakersMultipleLinesCheckbox.Enabled = false;
+                    BgWorker.RunWorkerAsync(new string[] {TextFileFolder, OutputFileLocation});
+                }
+            } 
+
+        }
+
+        
+
+
+
+
+        private void BgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            
+            //set up our sentence boundary detection
+            Regex NewlineClean = new Regex(@"[\r\n]+", RegexOptions.Compiled);
+
+            //selects the text encoding based on user selection
+            Encoding SelectedEncoding = null;
+            bool SpeakerMultipleLines = false;
+
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                SelectedEncoding = Encoding.GetEncoding(EncodingDropdown.SelectedItem.ToString());
+                SpeakerMultipleLines = SpeakersMultipleLinesCheckbox.Checked;
+
+            });
+
+
+
+
+            //the very first thing that we want to do is set up our speaker list
+
+            string[] SpeakerList = NewlineClean.Split(SpeakerListTextBox.Text);
+
+            //if we want things to be case-insensitive, this is what we'd do:
+            //string[] SpeakerListList = NewlineClean.Split(SpeakerListTextBox.Text.ToLower());
+
+            //remove blanks
+            SpeakerList = SpeakerList.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+
+
+
+            int SpeakerListLength = SpeakerList.Length;
+
+
+            //get the list of files
+            var SearchDepth = SearchOption.TopDirectoryOnly;
+            if (ScanSubfolderCheckbox.Checked)
+            {
+                SearchDepth = SearchOption.AllDirectories;
+            }
+            var files = Directory.EnumerateFiles( ((string[])e.Argument)[0], "*.txt", SearchDepth);
+
+            string outputFolder = System.IO.Path.Combine(((string[])e.Argument)[1], "ConverSplitter_Output");
+
+            try
+            {
+                System.IO.Directory.CreateDirectory(outputFolder);
+            }
+            catch
+            {
+                MessageBox.Show("ConverSplitterPlus could not create your output folder.\r\nIs your output directory write protected?");
+                e.Cancel = true;
+            }
+
+
+
+            try { 
+            
+               foreach (string fileName in files)
+                {
+
+
+
+                    //set up our variables to report
+                    string Filename_Clean = Path.GetFileName(fileName);
+
+                    Dictionary<string, string> Text_Split = new Dictionary<string, string>();
+
+
+                    //report what we're working on
+                    FilenameLabel.Invoke((MethodInvoker)delegate
+                    {
+                        FilenameLabel.Text = "Analyzing: " + Filename_Clean;
+                    });
+
+
+                    //do stuff here
+                    string[] readText_Lines = NewlineClean.Split(File.ReadAllText(fileName, SelectedEncoding));
+
+                    int NumberOfLines = readText_Lines.Length;
+
+
+                    //loop through all of the lines in each text
+
+                    string PreviousSpeaker = "";
+                    
+                    for (int i = 0; i < NumberOfLines; i++){
+
+                            string CurrentLine = readText_Lines[i].Trim();
+
+                            //if the line is empty, move along... move along
+                            if (CurrentLine.Length == 0)
+                            {
+                                continue;
+                            }
+
+                            bool FoundSpeaker = false;
+
+                            //loop through each speaker in list to see if the line starts with their name
+                            for (int j =0; j < SpeakerListLength; j++)
+                            {
+
+                                // here's what we do if we find a match
+                                if (CurrentLine.StartsWith(SpeakerList[j]))
+                                {
+
+                                    FoundSpeaker = true;
+                                    PreviousSpeaker = SpeakerList[j];
+
+                                    //clean up the line to remove the speaker tag from the beginning
+                                    int Place = CurrentLine.IndexOf(SpeakerList[j]);
+                                    CurrentLine = CurrentLine.Remove(Place, SpeakerList[j].Length).Insert(Place, "").Trim() + "\r\n";
+
+                                    if (Text_Split.ContainsKey(SpeakerList[j])){
+                                       
+                                        Text_Split[SpeakerList[j]] += CurrentLine;
+                                    }
+
+                                    else
+                                    {
+                                        Text_Split.Add(SpeakerList[j], CurrentLine);
+                                    }
+
+                                    //break to the next line in the text
+                                    break;
+                                }
+
+
+                            }
+
+
+                            //what we will do if no speaker was found
+                            if (FoundSpeaker == false)
+                            {
+                                if (SpeakerMultipleLines)
+                                {
+                                    Text_Split[PreviousSpeaker] += CurrentLine.Trim() + "\r\n";
+                                }
+                            }
+
+                    //end of for loop through each line
+                    }
+
+
+
+                    //here's where we want to write the output! hooray!
+                    foreach (KeyValuePair<string, string> entry in Text_Split)
+                    {
+
+                        string OutputFilename = Path.GetFileNameWithoutExtension(fileName) + ";" + entry.Key + ".txt";
+                        
+                        //clean up broken filenames
+                        foreach (var c in Path.GetInvalidFileNameChars()) { OutputFilename = OutputFilename.Replace(c, '_'); }
+
+                        //set the full path of our output
+                        OutputFilename = System.IO.Path.Combine(outputFolder, OutputFilename);
+
+                        // write the output
+                        using (StreamWriter outputFile = new StreamWriter(new FileStream(OutputFilename, FileMode.Create, FileAccess.Write), SelectedEncoding))
+                        {
+                            outputFile.Write(entry.Value);
+                        }
+                    }
+
+
+                    //end of for loop through each file
+                }
+
+
+          
+            //end of try block
+            }
+            catch
+            {
+                MessageBox.Show("ConverSplitterPlus could not open your output file\r\nfor writing. Is the file open in another application?");
+            }
+
+
+
+            
+        }
+
+        private void BgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            StartButton.Enabled = true;
+            SpeakerListTextBox.Enabled = true;
+            ScanSubfolderCheckbox.Enabled = true;
+            EncodingDropdown.Enabled = true;
+            SpeakersMultipleLinesCheckbox.Enabled = true;
+            FilenameLabel.Text = "Finished!";
+            MessageBox.Show("ConverSplitterPlus has finished analyzing your texts.", "Analysis Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+       
+
+    }
+    
+
+
+}
